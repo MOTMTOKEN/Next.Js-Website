86bda9dfc4640d8ad47795f990f357899dcc2b7d
MOTMTOKEN <amirnilsson3@gmail.com>
2022-01-08 21:46:46 +0100

    working website with Json data

---
+++ comps/Navbar.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <a href="/">
+        <Image src="/logo.png" alt="site logo" width={128} height={77} />
+        </a>
+        
+      </div>
+      <Link href="/"><a>Home</a></Link>
+      <Link href="/about"><a>About</a></Link>
+      <Link href="/ninjas/"><a>Ninja Listing</a></Link>
+    </nav>
+  );
+}
+ 
+export default Navbar;
+
